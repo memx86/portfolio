49abb2a3bc6e223f2b1d426534645da0bc6b0094
Yevhen Malyshko <memx86@gmail.com>
2022-04-23 00:25:03 +0300

    images for Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,16 +2,51 @@ import person from "assets/db/person";
 import s from "./Home.module.scss";
 
 function Home() {
-  const { name, lastname, images, summary } = person;
-  console.log("s", s);
+  const { name, lastname, photo, summary } = person;
+  const {
+    jpgS,
+    jpgS2x,
+    jpgS3x,
+    jpgL,
+    jpgL2x,
+    jpgL3x,
+    webpS,
+    webpS2x,
+    webpS3x,
+    webpL,
+    webpL2x,
+    webpL3x,
+  } = photo;
   return (
     <div className={s.container}>
-      <img
-        src={images.jpg}
-        alt={`${name} ${lastname}`}
-        width="280"
-        className={s.img}
-      />
+      <picture>
+        <source
+          srcSet={`${webpS} 1x, ${webpS2x} 2x, ${webpS3x} 3x`}
+          type="image/webp"
+          media="(max-width:767px)"
+        />
+        <source
+          srcSet={`${jpgS} 1x, ${jpgS2x} 2x, ${jpgS3x} 3x`}
+          type="image/jpeg"
+          media="(max-width:767px)"
+        />
+        <source
+          srcSet={`${webpL} 1x, ${webpL2x} 2x, ${webpL3x} 3x`}
+          type="image/webp"
+          media="(min-width:768px)"
+        />
+        <source
+          srcSet={`${jpgL} 1x, ${jpgL2x} 2x, ${jpgL3x} 3x`}
+          type="image/jpeg"
+          media="(min-width:768px)"
+        />
+        <img
+          src={jpgS}
+          alt={`${name} ${lastname}`}
+          width="280"
+          className={s.img}
+        />
+      </picture>
       <div className={s.content}>
         <p className={s.title}>
           <span className={s.block}> Hi, my name is</span>
